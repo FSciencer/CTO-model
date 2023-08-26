315cc85b72ff26d542e3604350e559c26fcec4d8
FSciencer <143240318+FSciencer@users.noreply.github.com>
2023-08-26 20:06:15 +0800

    Update samplers.py

--- samplers.py
+++ samplers.py
@@ -1,9 +1,3 @@
-# --------------------------------------------------------
-# Swin Transformer
-# Copyright (c) 2021 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# overwritten by authors
-# --------------------------------------------------------
 
 import torch
 
