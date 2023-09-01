5b0124c9697cf62b75724d227f8c13f34b95042b
FSciencer <143240318+FSciencer@users.noreply.github.com>
2023-09-01 13:20:38 +0800

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -1,5 +1,7 @@
 
 import os
+import cv2
+import random
 import numpy as np
 from matplotlib import pylab as plt
 from skimage import morphology
@@ -35,6 +37,138 @@ def filling(mask):
     dilate = binary_dilation(coronary, structure, iterations=1)
     filling_result = binary_erosion(dilate, structure, iterations=1)
     return filling_result - mask
+
+
+class Stack:
+    def __init__(self):
+        self.item = []
+        self.obj = []
+
+    def push(self, value):
+        self.item.append(value)
+
+    def pop(self):
+        return self.item.pop()
+
+    def size(self):
+        return len(self.item)
+
+    def isEmpty(self):
+        return self.size() == 0
+
+    def clear(self):
+        self.item = []
+
+
+class regionGrow:
+    def __init__(self, im_path, th):
+        self.readImage(im_path)
+        self.h, self.w, self.d = self.im.shape
+        self.passedBy = np.zeros((self.h, self.w, self.d), np.double)
+        self.currentRegion = 0
+        self.iterations = 0
+        self.SEGS = np.zeros((self.h, self.w, self.d, 3), dtype='uint8')
+        self.stack = Stack()
+        self.thresh = float(th)
+
+    def readImage(self, img_path):
+        self.im = cv2.imread(img_path, 1)
+
+    def getNeighbour(self, x0, y0, z0):
+        neighbour = []
+        for i in (-1, 0, 1):
+            for j in (-1, 0, 1):
+                for k in (-1, 0, 1):
+                    if (i, j, k) == (0, 0, 0):
+                        continue
+                    x = x0 + i
+                    y = y0 + j
+                    z = z0 + k
+                    if self.limit(x, y, z):
+                        neighbour.append((x, y, z))
+        return neighbour
+
+    def ApplyRegionGrow(self):
+        randomseeds = [[self.h / 2, self.w / 2, self.d / 2], [self.h / 3, self.w / 3, self.d / 3],
+                       [2 * self.h / 3, self.w / 3, self.d / 3], [self.h / 3, 2 * self.w / 3, self.d / 3], [self.h / 3, self.w / 3, 2 * self.d / 3],
+                       [self.h / 3 - 10, self.w / 3, self.d / 3], [self.h / 3, self.w / 3 - 10, self.d / 3], [self.h / 3, self.w / 3, self.d / 3 - 10],
+                       [self.h / 3 - 10, self.w / 3 - 10, self.d / 3], [self.h / 3, self.w / 3 - 10, self.d / 3 - 10], [self.h / 3 - 10, self.w / 3, self.d / 3 - 10],
+                       [self.h / 3 - 10, self.w / 3 - 10, self.d / 3 - 10],
+                       [2 * self.h / 3, 2 * self.w / 3, self.d / 3], [self.h / 3, 2 * self.w / 3, 2 * self.d / 3], [2 * self.h / 3, self.w / 3, 2 * self.d / 3],
+                       [2 * self.h / 3, 2 * self.w / 3, 2 * self.d / 3],
+                       [2 * self.h / 3, self.w / 3 - 10, self.d / 3 - 10], [self.h / 3 - 10, 2 * self.w / 3, self.d / 3 - 10], [self.h / 3 - 10, self.w / 3 - 10, 2 * self.d / 3],
+                       [2 * self.h / 3, 2 * self.w / 3, self.d / 3 - 10], [self.h / 3 - 10, 2 * self.w / 3, 2 * self.d / 3], [2 * self.h / 3, self.w / 3 - 10, 2 * self.d / 3],
+                       ]
+        np.random.shuffle(randomseeds)
+        for x0 in range(self.h):
+            for y0 in range(self.w):
+                for z0 in range(self.d):
+                    if self.passedBy[x0, y0, z0] == 0 and (int(self.im[x0, y0, z0, 0]) * int(self.im[x0, y0, z0, 1]) * int(self.im[x0, y0, z0, 2]) > 0):
+                        self.currentRegion += 1
+                        self.passedBy[x0, y0] = self.currentRegion
+                        self.stack.push((x0, y0))
+                        self.prev_region_count = 0
+                        while not self.stack.isEmpty():
+                            x, y, z = self.stack.pop()
+                            self.BFS(x, y, z)
+                            self.iterations += 1
+                        if self.PassedAll():
+                            break
+                        if self.prev_region_count < 8 * 8:
+                            self.passedBy[self.passedBy == self.currentRegion] = 0
+                            x0 = random.randint(x0 - 4, x0 + 4)
+                            y0 = random.randint(y0 - 4, y0 + 4)
+                            z0 = random.randint(z0 - 4, z0 + 4)
+                            x0 = max(0, x0)
+                            y0 = max(0, y0)
+                            z0 = max(0, z0)
+                            x0 = min(x0, self.h - 1)
+                            y0 = min(y0, self.w - 1)
+                            z0 = min(z0, self.d - 1)
+                            self.currentRegion -= 1
+
+        for i in range(0, self.h):
+            for j in range(0, self.w):
+                for k in range(0, self.d):
+                    val = self.passedBy[i][j][k]
+                    if val == 0:
+                        self.SEGS[i][j][k] = 255, 255, 255
+                    else:
+                        self.SEGS[i][j][k] = val * 35, val * 90, val * 30
+        if self.iterations > 1000:
+            print("Max Iterations")
+        print("Iterations : " + str(self.iterations))
+        cv2.imshow("", self.SEGS)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def BFS(self, x0, y0, z0):
+        regionNum = self.passedBy[x0, y0, z0]
+        elems = []
+        elems.append((int(self.im[x0, y0, z0, 0]) + int(self.im[x0, y0, z0, 1]) + int(self.im[x0, y0, z0, 2])) / 3)
+        var = self.thresh
+        neighbours = self.getNeighbour(x0, y0, z0)
+        for x, y, z in neighbours:
+            if self.passedBy[x, y, z] == 0 and self.distance(x, y, z, x0, y0, z0) < var:
+                if self.PassedAll():
+                    break
+                self.passedBy[x, y, z] = regionNum
+                self.stack.push((x, y, z))
+                elems.append((int(self.im[x, y, z, 0]) + int(self.im[x, y, z, 1]) + int(self.im[x, y, z, 2])) / 3)
+                var = np.var(elems)
+                self.prev_region_count += 1
+            var = max(var, self.thresh)
+
+    def PassedAll(self):
+        return self.iterations > 1000 or np.count_nonzero(self.passedBy > 0) == self.d * self.w * self.h
+
+    def limit(self, x, y, z):
+        return 0 <= x < self.h and 0 <= y < self.w and 0 <= z < self.d
+
+    def distance(self, x, y, z, x0, y0, z0):
+        return ((int(self.im[x, y, z, 0]) - int(self.im[x0, y0, z0, 0])) ** 2 +
+                (int(self.im[x, y, z, 1]) - int(self.im[x0, y0, z0, 1])) ** 2 +
+                (int(self.im[x, y, z, 2]) - int(self.im[x0, y0, z0, 2])) ** 2) ** 0.5
 
 
 def get_patch(img, mask, patchsize=64, overlap=0.2):
